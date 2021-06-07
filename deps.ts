b75bef76839348155292d3e247b3a906466e1023
PresentMonkey <gscience213@gmail.com>
2021-06-07 14:09:17 -0400

    Fix Issues

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/std@0.95.0/fmt/colors.ts";
 export * from "https://deno.land/x/sabr@1.1.4/mod.ts";
-export * from "https://raw.githubusercontent.com/discordeno/discordeno/main/mod.ts";
+export * from "https://deno.land/x/discordeno@11.0.0/mod.ts";
